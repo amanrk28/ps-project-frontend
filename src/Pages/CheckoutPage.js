3ce310598c4cb326c2c3f3dda00dc012b846313e
AmanRK <amankhemka.ak28@gmail.com>
2021-10-08 22:32:47 +0530

    fixed unused vars

--- src/Pages/CheckoutPage.js
+++ src/Pages/CheckoutPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/common/Header/Header';
 
-const CheckoutPage = ({ history, match }) => (
+const CheckoutPage = () => (
   <>
     <Header enableSearch />
   </>
